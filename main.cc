dba9339e26e6cedeb3933e87f4e0789e1bb050d2
APAC\NOG6HC <nog6hc@bosch.com>
2022-06-24 12:30:26 +0700

    first version

---
+++ main.cc
@@ -0,0 +1,16 @@
+/**
+ * @file main.cc
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-06-23
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+int main(int argc, char const *argv[])
+{
+  /* code */
+  return 0;
+}
